6d683b704470ac1bf2e08f2ba48b8ba368c87946
rafael <workpageraf@gmail.com>
2025-04-12 10:33:43 +0300

    "add flake8"

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils.encoding import force_str, smart_bytes, smart_str
+from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from rest_framework import serializers
 
